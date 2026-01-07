c09cdeea5461d3d4c9441f2a7ae1839ab40f8486
Oussama Elhassouni <oelhasso@c2r2p10.1337.ma>
2026-01-07 15:40:07 +0100

    finished ex01 cpp01

---
+++ Circle_04/Cpps/CPP_MODULE_01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oelhasso <oelhasso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/07 15:34:57 by oelhasso          #+#    #+#             */
+/*   Updated: 2026/01/07 15:35:34 by oelhasso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+// #include <string>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+    std::cout << "--- Addresses ---" << std::endl;
+    std::cout << "Address of string variable:  " << &str << std::endl;
+    std::cout << "Address held by stringPTR:   " << stringPTR << std::endl;
+    std::cout << "Address held by stringREF:   " << &stringREF << std::endl;
+    std::cout << std::endl;
+    std::cout << "--- Values ---" << std::endl;
+    std::cout << "Value of string variable:    " << str << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+    return 0;
+}
