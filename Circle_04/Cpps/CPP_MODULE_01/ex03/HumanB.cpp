4367db683aed331ddef7b80b1c39b9f826a4e008
Oussama Elhassouni <oelhasso@c1r5p7.1337.ma>
2026-01-09 21:07:42 +0100

    update ex03 cpp01

--- Circle_04/Cpps/CPP_MODULE_01/ex03/HumanB.cpp
+++ Circle_04/Cpps/CPP_MODULE_01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: oelhasso <oelhasso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 20:18:56 by oelhasso          #+#    #+#             */
-/*   Updated: 2026/01/07 20:18:58 by oelhasso         ###   ########.fr       */
+/*   Updated: 2026/01/09 17:59:34 by oelhasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 HumanB::HumanB(std::string name) : _name(name), _weapon(NULL) {}
 HumanB::~HumanB() {}
 
-void HumanB::setWeapon(Weapon& weapon) {
-    this->_weapon = &weapon;
+void HumanB::setWeapon(Weapon& weapon)
+{
+    _weapon = &weapon;
 }
 
 void HumanB::attack() {
-    if (this->_weapon)
+    if (_weapon)
         std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
     else
         std::cout << _name << " has no weapon!" << std::endl;
