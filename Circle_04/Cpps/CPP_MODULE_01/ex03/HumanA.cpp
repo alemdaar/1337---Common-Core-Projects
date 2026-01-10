0a0e220947c523e6e9ec6abdcc00e05fc12bd816
Oussama Elhassouni <oelhasso@c1r5p7.1337.ma>
2026-01-10 20:13:01 +0100

    finished cpp01

--- Circle_04/Cpps/CPP_MODULE_01/ex03/HumanA.cpp
+++ Circle_04/Cpps/CPP_MODULE_01/ex03/HumanA.cpp
@@ -6,15 +6,17 @@
 /*   By: oelhasso <oelhasso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 20:08:41 by oelhasso          #+#    #+#             */
-/*   Updated: 2026/01/07 20:18:36 by oelhasso         ###   ########.fr       */
+/*   Updated: 2026/01/10 17:07:58 by oelhasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
 HumanA::HumanA(std::string name, Weapon& weapon) : _name(name), _weapon(weapon) {}
+
 HumanA::~HumanA() {}
 
-void HumanA::attack() {
+void HumanA::attack()
+{
     std::cout << _name << " attacks with their " << _weapon.getType() << std::endl;
 }
